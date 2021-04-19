b01d117e7d554b5257f0fb9e9407fdf8d9e3ad7f
radvileram <radvileram@gmail.com>
2021-04-19 21:02:23 +0300

    Initial commit

---
+++ js/gallery.js
@@ -0,0 +1,204 @@
+function getGalleryLT(){
+    var gallery = [
+        {
+            title: "Baltas žiedas vynas raudonas",
+            description: "Drobė, 2015, 70x80",
+            image: "Images/baltasziedasvynasraudonas.jpg",
+        },
+        {
+            title: "Angelas su obuoliu",
+            description: "Drobė, 2019, 40x35",
+            image: "Images/angelassuobuoliu.jpg"
+        },
+        {
+            title: "Du smuikai",
+            description: "Drobė, 2019, 40x35",
+            image: "Images/dusmuikai.jpg",
+        },
+        {
+            title: "Kiemas",
+            description: "Drobė, 2009, 50x70",
+            image: "Images/kiemas.jpg",
+        },
+        {
+            title: "Kovas 30",
+            description: "Drobė, 2019, 30x25",
+            image: "Images/kovas30.jpg",
+        },
+        {
+            title: "Obels žiedai",
+            description: "Drobė, 2019, 50x40",
+            image: "Images/obelsziedai.jpg",
+        },
+        {
+            title: "Per daug atviras pokalbis",
+            description: "Drobė, 2018, 40x30",
+            image: "Images/perdaugatviraspokalbis.jpg",
+        },
+        {
+            title: "Ryto bučinys",
+            description: "Drobė, 2019, 50x40",
+            image: "Images/rytobucinis.jpg",
+        },
+        {
+            title: "Sodo paslaptys",
+            description: "Drobė, 2015, 60x70",
+            image: "Images/sodopaslaptys.jpg",
+        },
+        {
+            title: "Vyno taurė",
+            description: "Drobė, 2020, 20x20",
+            image: "Images/vynotaure.jpg",
+        },
+        {
+            title: "Žiedai prisiminti",
+            description: "Drobė, 2019, 40x30",
+            image: "Images/ziedaiprisiminti.jpg",
+        },
+        {
+            title: "Žiedų uola",
+            description: "Drobė, 2019, 50x40",
+            image: "Images/zieduuola.jpg",
+        }
+
+
+    ];
+    return gallery;
+}
+
+
+function getGalleryEN(){
+    var gallery = [
+        {
+            title: "White Ring Wine Red",
+            description: "Canvas, 2015, 70x80",
+            image: "Images/baltasziedasvynasraudonas.jpg",
+        },
+        {
+            title: "An Angel With an Apple",
+            description: "Canvas, 2019, 40x35",
+            image: "Images/angelassuobuoliu.jpg"
+        },
+        {
+            title: "Two Violins",
+            description: "Canvas, 2019, 40x35",
+            image: "Images/dusmuikai.jpg",
+        },
+        {
+            title: "Backyards",
+            description: "Canvas, 2009, 50x70",
+            image: "Images/kiemas.jpg",
+        },
+        {
+            title: "March 30",
+            description: "Canvas, 2019, 30x25",
+            image: "Images/kovas30.jpg",
+        },
+        {
+            title: "Apple Blossoms",
+            description: "Canvas, 2019, 50x40",
+            image: "Images/obelsziedai.jpg",
+        },
+        {
+            title: "Too Open Conversation",
+            description: "Canvas, 2018, 40x30",
+            image: "Images/perdaugatviraspokalbis.jpg",
+        },
+        {
+            title: "Morning Kiss",
+            description: "Canvas, 2019, 50x40",
+            image: "Images/rytobucinis.jpg",
+        },
+        {
+            title: "Garden Secrets",
+            description: "Canvas, 2015, 60x70",
+            image: "Images/sodopaslaptys.jpg",
+        },
+        {
+            title: "Wine Glass",
+            description: "Canvas, 2020, 20x20",
+            image: "Images/vynotaure.jpg",
+        },
+        {
+            title: "Blossoms to Remember",
+            description: "Canvas, 2019, 40x30",
+            image: "Images/ziedaiprisiminti.jpg",
+        },
+        {
+            title: "The Rock of The Flowers",
+            description: "Canvas, 2019, 50x40",
+            image: "Images/zieduuola.jpg",
+        }
+
+
+    ];
+    return gallery;
+}
+
+function getGalleryFR(){
+    var gallery = [
+        {
+            title: "Anneau blanc vin rouge",
+            description: "Toile, 2015, 70x80",
+            image: "Images/baltasziedasvynasraudonas.jpg",
+        },
+        {
+            title: "Un ange avec une pomme",
+            description: "Toile, 2019, 40x35",
+            image: "Images/angelassuobuoliu.jpg"
+        },
+        {
+            title: "Deux violons",
+            description: "Toile, 2019, 40x35",
+            image: "Images/dusmuikai.jpg",
+        },
+        {
+            title: "Cour arrière",
+            description: "Toile, 2009, 50x70",
+            image: "Images/kiemas.jpg",
+        },
+        {
+            title: "30 mars",
+            description: "Toile, 2019, 30x25",
+            image: "Images/kovas30.jpg",
+        },
+        {
+            title: "Fleurs de pommier",
+            description: "Toile, 2019, 50x40",
+            image: "Images/obelsziedai.jpg",
+        },
+        {
+            title: "Trop ouvrir une conversation",
+            description: "Toile, 2018, 40x30",
+            image: "Images/perdaugatviraspokalbis.jpg",
+        },
+        {
+            title: "Baiser du matin",
+            description: "Toile, 2019, 50x40",
+            image: "Images/rytobucinis.jpg",
+        },
+        {
+            title: "Secrets de jardin",
+            description: "Toile, 2015, 60x70",
+            image: "Images/sodopaslaptys.jpg",
+        },
+        {
+            title: "Un verre de vin",
+            description: "Toile, 2020, 20x20",
+            image: "Images/vynotaure.jpg",
+        },
+        {
+            title: "Anneaux à retenir",
+            description: "Toile, 2019, 40x30",
+            image: "Images/ziedaiprisiminti.jpg",
+        },
+        {
+            title: "Le rocher des fleurs",
+            description: "Toile, 2019, 50x40",
+            image: "Images/zieduuola.jpg",
+        }
+
+
+    ];
+    return gallery;
+}
