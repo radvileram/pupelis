853262259f3f2bc989e35fe30cc55bbd8a23d842
radvileram <radvileram@gmail.com>
2021-04-20 21:50:41 +0300

    home Paragraph

--- js/translate.js
+++ js/translate.js
@@ -7,7 +7,7 @@ function getlanguageTranslations(lang) {
       contacts: "KONTAKTAI",
       homeHeader: "Rimvydas Pupelis - MUTA",
       homeParagraph:
-        "Tapytojas Rimvydas Pupelis, gyvenantis ir kuriantis Lietuvos periferijoje – Obeliuose, tačiau parodas rengiantis plačiai – tiek Lietuvoje, tiek už šalies ribų.",
+        "Rimvydas Pupelis - tapytojas, scenografas, originalių meninių projektų autorius yra žinomas menininkas ne tik Lietuvoje bet ir užsienyje. Lietuvos dailės fone Rimvydo kūriniai turi labai atpažįstamą tapybos manierą – ryškumą, žaismingumą, emocijų pravokavimą spalvų praturtinimą.",
       aboutAuthor:
         "RIMVYDAS PUPELIS - tapytojas, gimė 1965 m. Obeliuose. 1984 m. baigė Šiaulių 13 profesinę technikos mokyklą. Studijavo Šiaulių universitete. 2006 m. baigė Šiaulių universiteto dailės ir technologijų studijas, įgijo mokytojo profesinę kvalifikaciją. Nuo 1997 m. Rokiškio dailininkų klubo „Roda“ narys. 2015 m. suteiktas meno kūrėjo statusas. Surengė 30 personalinių parodų. Aktyvus įvairių meno projektų organizatorius ir dalyvis. Dalyvauja parodose Lietuvoje ir užsienyje: Prancūzija, Šveicarija, Makedonija, Rumunija, Graikija, Latvija. Gyvena Obeliuose.",
       galleryHeader: "GALERIJA",
@@ -25,7 +25,7 @@ function getlanguageTranslations(lang) {
       contacts: "CONTACTS",
       homeHeader: "Rimvydas Pupelis - MUTA",
       homeParagraph:
-        "Painter Rimvydas Pupelis, who lives and works in the Lithuanian periphery - Obeliai, but organizes exhibitions extensively - both in Lithuania and abroad.",
+        "Rimvydas Pupelis - painter, scenographer, author of original artistic projects is a well-known artist not only in Lithuania but also abroad. Against the background of Lithuanian art, Rimvydas' works have a very recognizable manner of painting - brightness, playfulness, anticipation of emotions and enrichment of colors.",
       aboutAuthor:
         "RIMVYDAS PUPELIS - painter, born in 1965 in Obeliai. 1984 graduated from Šiauliai 13 vocational technical school. Studied at Šiauliai University. 2006 graduated from Šiauliai University with a degree in art and technology, acquired a professional qualification of a teacher. Since 1997 Member of Rokiškis artists' club “Roda“. 2015 granted the status of an artist. Held 30 solo exhibitions. Active organizer and participant of various art projects. Participates in exhibitions in Lithuania and abroad: France, Switzerland, Macedonia, Romania, Greece, Latvia. Lives in Obeliai.",
       galleryHeader: "GALLERY",
@@ -42,7 +42,7 @@ function getlanguageTranslations(lang) {
       contacts: "CONTACTS",
       homeHeader: "Rimvydas Pupelis - MUTA",
       homeParagraph:
-        "Peintre Rimvydas Pupelis, qui vit et travaille dans la périphérie lituanienne - Obeliai, mais organise de nombreuses expositions - en Lituanie et à l'étranger.",
+        "Rimvydas Pupelis - peintre, scénographe, auteur de projets artistiques originaux est un artiste bien connu non seulement en Lituanie mais aussi à l'étranger. Dans le contexte de l'art lituanien, les œuvres de Rimvydas ont une manière de peindre très reconnaissable - luminosité, espièglerie, anticipation des émotions et enrichissement des couleurs.",
       aboutAuthor:
         "RIMVYDAS PUPELIS - peintre, né en 1965 à Obeliai. 1984 diplômé de l'école technique professionnelle Šiauliai 13. A étudié à l'Université de Šiauliai. 2006 diplômé de l'Université de Šiauliai avec un diplôme en art et technologie, a acquis une qualification professionnelle d'enseignant. Depuis 1997 Membre du club d'artistes Rokiškis “Roda“. 2015 a accordé le statut d'artiste. Tenu 30 expositions personnelles. Organisateur actif et participant de divers projets artistiques. Participe à des expositions en Lituanie et à l'étranger: France, Suisse, Macédoine, Roumanie, Grèce, Lettonie. Vit à Obeliai.",
       galleryHeader: "GALERIE",
